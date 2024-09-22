33ee8ba7d7e720032a1d6fecfc7390b257370b29
Isha Negi <itsishanegi@gmail.com>
2024-09-22 17:03:56 +0530

    Add files via upload

---
+++ smallest-element-in-array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{ int x[6]={432,553,878,7676,767,32};
+
+int min=x[0];
+
+int i;
+  
+for(i=0;i<6;i++)
+  
+{ if(min>x[i]) 
+    
+{ 
+      
+min=x[i];
+    
+}
+  
+}
+
+printf("[%d]= %d --> is the smallest in array ",i,min);
+
+}
